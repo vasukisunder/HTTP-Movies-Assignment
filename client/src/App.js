20f16d294335a71f470222b2bb70d34a758f3117
Vasuki Sunder <vasukisunder@Vasukis-MBP.attlocal.net>
2020-04-22 23:56:55 -0700

    done

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
 import axios from 'axios';
+import Update from "./Movies/Update";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -33,7 +34,11 @@ const App = () => {
       </Route>
 
       <Route path="/movies/:id">
-        <Movie addToSavedList={addToSavedList} />
+        <Movie addToSavedList={addToSavedList} getMovieList = {getMovieList}/>
+      </Route>
+
+      <Route path="/update-movie/:id">
+        <Update getMovieList = {getMovieList} />
       </Route>
     </>
   );
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
-function Movie({ addToSavedList }) {
+function Movie({ addToSavedList, getMovieList }) {
   const [movie, setMovie] = useState(null);
   const params = useParams();
+
+  const { push } = useHistory();
 
   const fetchMovie = (id) => {
     axios
@@ -17,6 +19,19 @@ function Movie({ addToSavedList }) {
   const saveMovie = () => {
     addToSavedList(movie);
   };
+
+  const deleteMovie = e => {
+    e.preventDefault();
+    axios
+    .delete(`http://localhost:5000/api/movies/${movie.id}`)
+    .then( res => {
+      console.log(res);
+      getMovieList();
+      push('/');
+    })
+
+  }
+
 
   useEffect(() => {
     fetchMovie(params.id);
@@ -30,9 +45,15 @@ function Movie({ addToSavedList }) {
     <div className="save-wrapper">
       <MovieCard movie={movie} />
 
-      <div className="save-button" onClick={saveMovie}>
+      <button className="save-button" onClick={saveMovie}>
         Save
-      </div>
+      </button>
+      <button  className="save-button" onClick={() => push(`/update-movie/${params.id}`)}>
+        Update
+      </button>
+      <button  className="save-button" onClick={deleteMovie}>
+        Delete
+      </button>
     </div>
   );
 }
